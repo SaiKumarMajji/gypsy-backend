3d1bde3027aba3642b17843894094655bd863e66
SaiKumarMajji <saikumar170325@gmail.com>
2025-10-19 10:01:08 +0530

    some changes

--- src/utils/email.util.ts
+++ src/utils/email.util.ts
@@ -1,10 +1,11 @@
 import nodemailer from "nodemailer";
+import { Resend } from "resend";
 
 const transporter = nodemailer.createTransport({
- service: "Gmail",        
+  service: "Gmail",
   auth: {
-    user: process.env.EMAIL_USER, 
-    pass: process.env.EMAIL_PASS, 
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
@@ -13,14 +14,27 @@ transporter.verify((error, success) => {
   else console.log("SMTP server ready");
 });
 
+const resend = new Resend(process.env.RESEND_API_KEY);
+// export const sendEmail = async (to: string | string[], subject: string, html: string) => {
+//   const mailOptions = {
+//     from: `"Gypsy Wings" <${process.env.EMAIL_USER}>`,
+//     to, 
+//     subject,
+//     html,
+//   };
 
-export const sendEmail = async (to: string | string[], subject: string, html: string) => {
-  const mailOptions = {
-    from: `"Gypsy Wings" <${process.env.EMAIL_USER}>`,
-    to, 
-    subject,
-    html,
-  };
+//   return transporter.sendMail(mailOptions);
+// };
 
-  return transporter.sendMail(mailOptions);
+export const sendEmail = async (to: string[], subject: string, html: string) => {
+  try {
+    await resend.emails.send({
+      from: "Gypsy Aviators <noreply@gypsyaviators.com>",
+      to,
+      subject,
+      html,
+    });
+  } catch (error) {
+    console.error("❌ Resend email failed:", error);
+  }
 };
