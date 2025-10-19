62a265859a77c8fdba2c0a84fd5d9b375360e96e
SaiKumarMajji <saikumar170325@gmail.com>
2025-10-19 10:01:58 +0530

    minor changes

--- src/utils/email.util.ts
+++ src/utils/email.util.ts
@@ -1,18 +1,6 @@
 import nodemailer from "nodemailer";
 import { Resend } from "resend";
 
-const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
-
-transporter.verify((error, success) => {
-  if (error) console.log("SMTP error:", error);
-  else console.log("SMTP server ready");
-});
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 // export const sendEmail = async (to: string | string[], subject: string, html: string) => {
