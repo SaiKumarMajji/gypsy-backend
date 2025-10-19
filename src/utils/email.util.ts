df2bc1d97a97e9bc7a1fad9a322b75386d2d84de
SaiKumarMajji <saikumar170325@gmail.com>
2025-10-19 10:15:53 +0530

    recheck

--- src/utils/email.util.ts
+++ src/utils/email.util.ts
@@ -3,21 +3,11 @@ import { Resend } from "resend";
 
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-// export const sendEmail = async (to: string | string[], subject: string, html: string) => {
-//   const mailOptions = {
-//     from: `"Gypsy Wings" <${process.env.EMAIL_USER}>`,
-//     to, 
-//     subject,
-//     html,
-//   };
-
-//   return transporter.sendMail(mailOptions);
-// };
 
 export const sendEmail = async (to: string[], subject: string, html: string) => {
   try {
     await resend.emails.send({
-      from: "Gypsy Aviators <noreply@gypsyaviators.com>",
+      from: "Gypsy Aviators <onboarding@resend.dev>",
       to,
       subject,
       html,
